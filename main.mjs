376059c69750aaffc8730d263e46e9c1ddfd9f45
bekbolat2004 <bekbolatbekbolat32@gmail.com>
2023-03-05 20:47:00 +0600

    added js and json file

---
+++ main.mjs
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const main = async () => {
+  const response = saxios.get("http://localhost:3000/products/1");
+  console.log((await response).data);
+};
+
+main().catch((error) => console.log(error));
